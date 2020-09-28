115af4542361085aee61af7c31cc64cbe1ab4667
Nathaniel Evan <nathanielevan@zohomail.com>
2020-09-29 06:01:53 +0700

    Fixed screenshot scripts to hide the cursor

--- .local/bin/screenshot_sel.sh
+++ .local/bin/screenshot_sel.sh
@@ -3,6 +3,6 @@
 # Script to take screenshot, put on clipboard and save it in screenshot folder
 
 SCREENSHOT_FILE=~/Pictures/screenshots/$(date +%F-%H%M%S).png
-maim -s | xclip -selection clipboard -t image/png
+maim -u -s | xclip -selection clipboard -t image/png
 xclip -selection clipboard -t image/png -o > "$SCREENSHOT_FILE"
 notify-send 'Screenshot taken!' "Saved at $SCREENSHOT_FILE" --icon=camera-photo
