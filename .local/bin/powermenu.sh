2aac6fd534c185cb2ef7cd988205310790c84cbb
Nathaniel Evan <nathanielevan@zohomail.com>
2020-10-03 16:17:54 +0700

    Fixed power menu script; use systemctl to suspend, let xss-lock handle the locking

--- .local/bin/powermenu.sh
+++ .local/bin/powermenu.sh
@@ -7,7 +7,7 @@ option=$(echo -e "lock\nlogout\nsuspend\nreboot\nshutdown" | rofi -dmenu -disabl
 case "$option" in
         lock) betterlockscreen -l blur 0.5 ;;
         logout) bspc quit ;;
-        suspend) betterlockscreen -s blur 0.5 ;;
+        suspend) systemctl suspend ;;
         reboot) systemctl reboot ;;
         shutdown) systemctl poweroff ;;
 esac
