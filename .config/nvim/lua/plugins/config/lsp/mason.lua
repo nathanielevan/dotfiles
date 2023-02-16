f4392d14ab996cbbe51ad85fe766869abcc840c7
Nathaniel Evan <nathanielevan@zohomail.com>
2023-02-16 11:15:34 +0700

    nvim: Remove default settings from mason.setup

--- .config/nvim/lua/plugins/config/lsp/mason.lua
+++ .config/nvim/lua/plugins/config/lsp/mason.lua
@@ -4,53 +4,6 @@ if not status_ok then
 end
 
 mason.setup {
-    -- The directory in which to install packages.
-    install_root_dir = vim.fn.stdpath ('data') .. 'mason',
-
-    -- Where Mason should put its bin location in your PATH. Can be one of:
-    -- - "prepend" (default, Mason's bin location is put first in PATH)
-    -- - "append" (Mason's bin location is put at the end of PATH)
-    -- - "skip" (doesn't modify PATH)
-    ---@type '"prepend"' | '"append"' | '"skip"'
-    PATH = "prepend",
-
-    pip = {
-        -- Whether to upgrade pip to the latest version in the virtual environment before installing packages.
-        upgrade_pip = false,
-
-        -- These args will be added to `pip install` calls. Note that setting extra args might impact intended behavior
-        -- and is not recommended.
-        --
-        -- Example: { "--proxy", "https://proxyserver" }
-        install_args = {},
-    },
-
-    -- Controls to which degree logs are written to the log file. It's useful to set this to vim.log.levels.DEBUG when
-    -- debugging issues with package installations.
-    log_level = vim.log.levels.INFO,
-
-    -- Limit for the maximum amount of packages to be installed at the same time. Once this limit is reached, any further
-    -- packages that are requested to be installed will be put in a queue.
-    max_concurrent_installers = 4,
-
-    github = {
-        -- The template URL to use when downloading assets from GitHub.
-        -- The placeholders are the following (in order):
-        -- 1. The repository (e.g. "rust-lang/rust-analyzer")
-        -- 2. The release version (e.g. "v0.3.0")
-        -- 3. The asset name (e.g. "rust-analyzer-v0.3.0-x86_64-unknown-linux-gnu.tar.gz")
-        download_url_template = "https://github.com/%s/releases/download/%s/%s",
-    },
-
-    -- The provider implementations to use for resolving package metadata (latest version, available versions, etc.).
-    -- Accepts multiple entries, where later entries will be used as fallback should prior providers fail.
-    -- Builtin providers are:
-    --   - mason.providers.registry-api (default) - uses the https://api.mason-registry.dev API
-    --   - mason.providers.client                 - uses only client-side tooling to resolve metadata
-    providers = {
-        "mason.providers.registry-api",
-    },
-
     ui = {
         -- Whether to automatically check for new versions when opening the :Mason window.
         check_outdated_packages_on_open = true,
