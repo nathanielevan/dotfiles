0798b84d8523f47632b8cd5dd4bdc60389c20626
Nathaniel Evan <nathanielevan@zohomail.com>
2023-03-03 14:55:01 +0700

    nvim: Move mason keybind to core/keymaps

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -109,4 +109,27 @@ M.telescope = function()
     vim.api.nvim_set_keymap('n', '<leader>fo', ':Telescope oldfiles<CR>', { noremap = true, silent = true })
 end
 
+M.mason = function()
+    return {
+        -- Keymap to expand a package
+        toggle_package_expand = "<CR>",
+        -- Keymap to install the package under the current cursor position
+        install_package = "i",
+        -- Keymap to reinstall/update the package under the current cursor position
+        update_package = "<C-u>",
+        -- Keymap to check for new version for the package under the current cursor position
+        check_package_version = "c",
+        -- Keymap to update all installed packages
+        update_all_packages = "U",
+        -- Keymap to check which installed packages are outdated
+        check_outdated_packages = "C",
+        -- Keymap to uninstall a package
+        uninstall_package = "X",
+        -- Keymap to cancel a package installation
+        cancel_installation = "<C-c>",
+        -- Keymap to apply language filter
+        apply_language_filter = "<C-f>",
+    }
+end
+
 return M
