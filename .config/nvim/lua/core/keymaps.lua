e1d509e20d048e29ff66af523394885ecaa34c26
Nathaniel Evan <nathanielevan@zohomail.com>
2023-06-14 06:18:25 +0700

    nvim: Add keymap for luasnip navigation

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -135,4 +135,18 @@ M.mason = function()
     }
 end
 
+M.luasnip = function()
+    local ls = require('luasnip')
+    vim.keymap.set({ 'i', 's' }, "<C-k>", function()
+        if ls.expand_or_jumpable() then
+            ls.expand_or_jump()
+        end
+    end, { silent = true })
+    vim.keymap.set({ 'i', 's' }, "<C-j>", function()
+        if ls.jumpable(-1) then
+            ls.jump(-1)
+        end
+    end, { silent = true })
+end
+
 return M
