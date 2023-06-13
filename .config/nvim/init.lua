41a018e75e6e13cd647c8be1e1ea1f20551c32d3
Nathaniel Evan <nathanielevan@zohomail.com>
2023-06-14 04:42:45 +0700

    nvim: Switch from packer to lazy, remove impatient.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,2 @@
-local status_ok, impatient = pcall(require, "impatient")
-
-if status_ok then
-    impatient.enable_profile()
-end
-
 require("core")
 require("plugins")
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,126 +1,117 @@
-local fn = vim.fn
+local install_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system {
-        'git',
-        'clone',
-        '--depth',
-        '1',
-        'https://github.com/wbthomason/packer.nvim',
-        install_path
-    }
+if not vim.loop.fs_stat(install_path) then
+    LAZY_BOOTSTRAP = vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        install_path,
+    })
 end
 
-vim.cmd('packadd packer.nvim')
-local status_ok, packer = pcall(require, "packer")
+vim.opt.rtp:prepend(install_path)
+
+local status_ok, lazy = pcall(require, "lazy")
 
 if not status_ok then
-    print "Unable to load packer"
+    print "Unable to load lazy.nvim"
     return
 end
 
-packer.init {
-    git = {
-        clone_timeout = 600,
-    },
-    display = {
-        open_fn = function()
-            return require("packer.util").float { border = "rounded" }
+lazy.setup({
+    { 'sainnhe/gruvbox-material',
+        config = function()
+            require('plugins.config.gruvbox-material')
         end,
     },
-}
-
-function lazy_load(plugin, timer)
-    if plugin then
-        local timer = timer or 0
-        vim.defer_fn(function()
-            require("packer").loader(plugin)
-        end, timer)
-    end
-end
-
-return packer.startup(function(use)
-    use { 'lewis6991/impatient.nvim' }
-    use { 'nvim-lua/plenary.nvim',
-        module = 'plenary',
-    }
-    use { 'nathom/filetype.nvim' }
-    use { 'wbthomason/packer.nvim' }
-    use { 'dstein64/vim-startuptime' }
-    use { 'sainnhe/gruvbox-material',
-        event = "VimEnter",
-        config = "require('plugins.config.gruvbox-material')",
-    }
-    use { 'kyazdani42/nvim-web-devicons',
-        module = 'nvim-web-devicons',
-    }
-    use { 'nvim-treesitter/nvim-treesitter',
-        event = { 'BufRead', 'BufNewFile' },
-        run = ':TSUpdate',
-        config = "require('plugins.config.nvim-treesitter')",
-    }
-    use { 'lewis6991/gitsigns.nvim',
-        opt = true,
-        setup = function()
-            lazy_load('gitsigns.nvim')
+    { 'nvim-tree/nvim-web-devicons',
+        lazy = true,
+    },
+    { 'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        config = function()
+            require('plugins.config.nvim-treesitter')
         end,
-        config = "require('plugins.config.gitsigns')",
-    }
-    use { 'kyazdani42/nvim-tree.lua',
-        cmd = { 'NvimTreeToggle', 'NvimTreeFocus' },
-        setup = "require('core.keymaps').nvimtree()",
-        config = "require('plugins.config.nvim-tree')",
-    }
-    use { 'nvim-telescope/telescope.nvim',
-        cmd = 'Telescope',
-        setup = "require('core.keymaps').telescope()",
-        config = "require('plugins.config.telescope')",
-    }
-    use { 'goolord/alpha-nvim',
-        config = "require('plugins.config.alpha-nvim')",
-    }
-    use { 'williamboman/mason.nvim',
-        config = "require('plugins.config.lsp.mason')",
-    }
-    use { 'williamboman/mason-lspconfig.nvim',
-        after = 'mason.nvim',
-        config = "require('plugins.config.lsp.mason-lspconfig')",
-    }
-    use { 'neovim/nvim-lspconfig',
-        after = 'mason-lspconfig.nvim',
-        config = "require('plugins.config.lsp')"
-    }
-    use { 'rafamadriz/friendly-snippets',
+    },
+    { 'lewis6991/gitsigns.nvim',
+        config = function()
+            require('plugins.config.gitsigns')
+        end,
+    },
+    { 'nvim-tree/nvim-tree.lua',
+        cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        init = function()
+            require('core.keymaps').nvimtree()
+        end,
+        config = function()
+            require('plugins.config.nvim-tree')
+        end,
+    },
+    { 'nvim-telescope/telescope.nvim',
+        cmd = "Telescope",
+        dependencies = 'nvim-lua/plenary.nvim',
+        init = function()
+            require('core.keymaps').telescope()
+        end,
+        config = function()
+            require('plugins.config.telescope')
+        end,
+    },
+    { 'goolord/alpha-nvim',
+        config = function()
+            require('plugins.config.alpha-nvim')
+        end,
+    },
+    { 'williamboman/mason.nvim',
+        config = function()
+            require('plugins.config.lsp.mason')
+        end,
+    },
+    { 'williamboman/mason-lspconfig.nvim',
+        dependencies = 'williamboman/mason.nvim',
+        config = function()
+            require('plugins.config.lsp.mason-lspconfig')
+        end,
+    },
+    { 'neovim/nvim-lspconfig',
+        dependencies = 'williamboman/mason-lspconfig.nvim',
+        config = function()
+            require('plugins.config.lsp')
+        end,
+    },
+    { 'hrsh7th/nvim-cmp',
         event = "InsertEnter",
+        dependencies = {
+            { 'L3MON4D3/LuaSnip',
+                dependencies = 'rafamadriz/friendly-snippets',
+                config = function()
+                    require('core.keymaps').luasnip()
+                    require('plugins.config.luasnip')
+                end,
+            },
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lua',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+        },
+        config = function()
+            require('plugins.config.nvim-cmp')
+        end,
+    },
+},
+{
+    git = {
+        timeout = 120,
+    },
+    ui = {
+        border = "rounded",
     }
-    use { 'hrsh7th/nvim-cmp',
-        after = 'friendly-snippets',
-        config = "require('plugins.config.nvim-cmp')",
-    }
-    use { 'L3MON4D3/LuaSnip',
-        wants = 'friendly-snippets',
-        after = 'nvim-cmp',
-        config = "require('plugins.config.luasnip')",
-    }
-    use { 'saadparwaiz1/cmp_luasnip',
-        after = 'LuaSnip',
-    }
-    use { 'hrsh7th/cmp-nvim-lua',
-        after = 'nvim-cmp',
-    }
-    use { 'hrsh7th/cmp-nvim-lsp',
-        after = 'nvim-cmp',
-    }
-    use { 'hrsh7th/cmp-buffer',
-        after = 'nvim-cmp',
-    }
-    use { 'hrsh7th/cmp-path',
-        after = 'nvim-cmp',
-    }
+})
 
-    if PACKER_BOOTSTRAP then
-        require('packer').sync()
-    end
-end)
+if LAZY_BOOTSTRAP then
+    lazy.sync()
+end
