533bb199a9eb6602a994ba0b7dbdfcc341d4f0c9
Nathaniel Evan <nathanielevan@zohomail.com>
2023-06-14 16:46:59 +0700

    nvim: Remove deprecated nvim-tree settings

--- .config/nvim/lua/plugins/config/nvim-tree.lua
+++ .config/nvim/lua/plugins/config/nvim-tree.lua
@@ -13,9 +13,6 @@ nvimtree.setup {
     hijack_cursor = false,
     hijack_netrw = true,
     hijack_unnamed_buffer_when_opening = false,
-    ignore_buffer_on_setup = false,
-    open_on_setup = false,
-    open_on_setup_file = false,
     open_on_tab = false,
     sort_by = "name",
     update_cwd = false,
@@ -100,7 +97,6 @@ nvimtree.setup {
         update_cwd = false,
         ignore_list = {},
     },
-    ignore_ft_on_setup = {},
     system_open = {
         cmd = "",
         args = {},
