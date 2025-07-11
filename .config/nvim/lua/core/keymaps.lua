bb0c69eb6df8a1a05adea85dac19bba334519b50
Nathaniel Evan <nathanielevan@zohomail.com>
2025-07-11 21:31:14 +0700

    fix: nvim: Defer LuaSnip loading until triggered by keymap

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -188,14 +188,13 @@ M.mason = function()
 end
 
 M.luasnip = function()
-  local ls = require('luasnip')
-  vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
-  vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
-  vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
+  vim.keymap.set({ "i" }, "<C-k>", function() require('luasnip').expand() end, { silent = true })
+  vim.keymap.set({ "i", "s" }, "<C-l>", function() require('luasnip').jump(1) end, { silent = true })
+  vim.keymap.set({ "i", "s" }, "<C-j>", function() require('luasnip').jump(-1) end, { silent = true })
 
-  vim.keymap.set({ "i", "s" }, "<C-E>", function()
-    if ls.choice_active() then
-      ls.change_choice(1)
+  vim.keymap.set({ "i", "s" }, "<C-e>", function()
+    if require('luasnip').choice_active() then
+      require('luasnip').change_choice(1)
     end
   end, { silent = true })
 end
