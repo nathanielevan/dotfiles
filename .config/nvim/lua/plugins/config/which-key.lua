9b479db284aff32cd3b3a0744cbc3867e6c28b0f
Nathaniel Evan <nathanielevan@zohomail.com>
2025-07-15 04:44:24 +0700

    feat: nvim: Add which-key plugin and basic config

---
+++ .config/nvim/lua/plugins/config/which-key.lua
@@ -0,0 +1,42 @@
+return {
+  "folke/which-key.nvim",
+  -- triggered after LazyDone and processing VimEnter auto commands
+  -- see :help lazy.nvim-🚀-usage-📆-user-events for more details
+  event = "VeryLazy",
+  opts = {
+    preset = "helix",
+    icons = {
+      mappings = false,
+      keys = {
+        Up = '<Up> ',
+        Down = '<Down> ',
+        Left = '<Left> ',
+        Right = '<Right> ',
+        C = '<C-…> ',
+        M = '<M-…> ',
+        D = '<D-…> ',
+        S = '<S-…> ',
+        CR = '<CR> ',
+        Esc = '<Esc> ',
+        ScrollWheelDown = '<ScrollWheelDown> ',
+        ScrollWheelUp = '<ScrollWheelUp> ',
+        NL = '<NL> ',
+        BS = '<BS> ',
+        Space = '<Space> ',
+        Tab = '<Tab> ',
+        F1 = '<F1>',
+        F2 = '<F2>',
+        F3 = '<F3>',
+        F4 = '<F4>',
+        F5 = '<F5>',
+        F6 = '<F6>',
+        F7 = '<F7>',
+        F8 = '<F8>',
+        F9 = '<F9>',
+        F10 = '<F10>',
+        F11 = '<F11>',
+        F12 = '<F12>',
+      },
+    },
+  }
+}
