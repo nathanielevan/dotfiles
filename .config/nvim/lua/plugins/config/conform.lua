036cc0694ee6ac3b8c31536abbcd161e1b5c9e20
Nathaniel Evan <nathanielevan@zohomail.com>
2025-12-05 00:31:24 +0700

    fix(nvim): Make conform.nvim use prettier instead of prettierd
    This is because apparently prettierd doesn't kill itself when quitting,
    leading to many spawned processes when opening new nvim instances many
    times. Also make sure that javascriptreact, typescript and
    typescriptreact also use prettier to format code.

--- .config/nvim/lua/plugins/config/conform.lua
+++ .config/nvim/lua/plugins/config/conform.lua
@@ -7,7 +7,10 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
-      javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
     },
     -- Override built-in formatters or define custom formatters here
     -- formatters = {
